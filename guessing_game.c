bb658b3458302b98ca91659fe55a4495fdcdc64c
Patrick Sweeney <patrick.sweeney1983@gmail.com>
2025-08-01 12:50:21 +0100

    Refactored into functional program

--- guessing_game.c
+++ guessing_game.c
@@ -4,26 +4,41 @@
 
 #define MAX_TRIES 10
 
-int main()
+int generate_secret(void)
 {
-    //get random number
     srand(time(NULL));
     int secret = rand() % 100 + 1;
-    int guess=0, count = 0;
+    return secret;
+}
     
-    printf("I'm thinking of a number between 1 and 100.\n");
+int get_guess(void)
+{    
+    int guess;
+    printf("Take a guess: ");
+    scanf("%d", &guess);
+    return guess;
+}
 
+void play_game(void)
+{
+    int secret = generate_secret();
+    int guess = 0;
+    int count = 0;
+    printf("I'm thinking of a number between 1 and 100");
     while ( guess != secret && count < MAX_TRIES )
     {
-        printf("Take a guess: ");
-        scanf("%d", &guess);
-        count += 1;
+        guess = get_guess();
+        count++;
         if (guess > secret) printf("Too High\n");
         else if (guess <  secret) printf("Too Low\n");
     }
     if (count >= 10) printf("You've run out of tries, the correct answer was %d.", secret);
     else printf("Correct! You got it in %d tries.\n", count);
+}
 
+int main()
+{
+    play_game();
     return 0;
 }    
 
